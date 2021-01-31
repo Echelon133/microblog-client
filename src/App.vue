93f0bfe6b7c860f9be8a695d1cfd754aa18a44f9
Mateusz <echelon133@gmail.com>
2021-01-31 15:24:13 +0100

    No longer wrap router-view and remove default #app styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <b-container fluid id="app">
-    <router-view/>
-  </b-container>
+  <router-view/>
 </template>
 
 <script>
@@ -10,13 +8,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
