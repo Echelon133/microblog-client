9cef591cb91add22aaae666bb567218437b30a1f
Mateusz <echelon133@gmail.com>
2021-02-01 22:44:10 +0100

    Add new post form (with basic validation) at the top of the Feed component

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,16 +1,47 @@
 <template>
-  <h1>Feed</h1>
+  <div class="pt-5">
+    <b-row>
+      <b-col xl="9" offset-xl="1">
+        <div class="new-post-input">
+          <label for="post-content" class="form-label">Treść nowego wpisu: </label>
+          <span class="length-counter">{{ newPostContent.length }}/{{ maxPostLength }}</span>
+          <div class="input-group">
+            <b-form-textarea v-model="newPostContent" class="form-control" id="post-content" rows="4"></b-form-textarea>
+            <b-button variant="secondary"
+            :disabled="newPostContent.length === 0 || newPostContent.length > maxPostLength"
+            @click.prevent="onNewPost"
+            >Wyślij
+            </b-button>
+          </div>
+        </div>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Feed'
+  name: 'Feed',
+  components: {
+  },
+  data () {
+    return {
+      newPostContent: '',
+      maxPostLength: 300
+    }
+  },
+  methods: {
+    onNewPost () {
+      console.log(this.newPostContent)
+      this.newPostContent = ''
+    }
+  }
 }
 </script>
 
 <style scoped>
-h1 {
-  padding-top:200px;
-  text-align: center;
+.length-counter {
+  float: right;
 }
 </style>
