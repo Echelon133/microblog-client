6ebab20d72ad042ed9c4ec71bb8ce961bda92f19
Mateusz <echelon133@gmail.com>
2021-02-02 22:15:40 +0100

    Add Post component with placeholder template

---
+++ src/components/Post.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-row>
+    <b-col>
+      <p>{{ post.author.displayedUsername }}</p>
+      <p>{{ post.author.username }}</p>
+      <p>{{ post.content }}</p>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: ['post']
+}
+</script>
