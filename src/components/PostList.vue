6960715bbad655f3707c383c22053586d6b6523d
Mateusz <echelon133@gmail.com>
2021-02-02 22:22:44 +0100

    Implement PostList template (with Post looping)

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>List of posts - placeholder</h1>
+  <b-row class="posts">
+    <b-col xl="9" offset-xl="1" class="text-center pt-5" v-if="posts.length === 0">
+      <span class="no-content-text">Brak postów</span>
+    </b-col>
+    <b-col xl="9" offset-xl="1" class="text-center pt-5" v-else>
+      <Post v-for="post in posts" :key="post.uuid" :post="post"/>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
+import Post from '@/components/Post'
+
 export default {
-  name: 'PostList'
+  name: 'PostList',
+  props: ['posts'],
+  components: {
+    Post
+  }
 }
 </script>
+
+<style scoped>
+.no-content-text {
+  font-size: 40px;
+}
+</style>
