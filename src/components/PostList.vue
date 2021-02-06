715daa2da66681b5ffde84426288b612b1fae9d8
Mateusz <echelon133@gmail.com>
2021-02-06 15:03:44 +0100

    Change PostList's columns size to sm=9 to fix scaling on smaller screens

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,9 @@
 <template>
   <b-row class="posts">
-    <b-col xl="9" offset-xl="1" class="text-center pt-5" v-if="posts.length === 0">
+    <b-col sm="9" offset-sm="1" class="text-center pt-5" v-if="posts.length === 0">
       <span class="no-content-text">Brak postów</span>
     </b-col>
-    <b-col xl="9" offset-xl="1" class="pt-5" v-else>
+    <b-col sm="9" offset-sm="1" class="pt-5" v-else>
       <Post v-for="post in posts" :key="post.uuid" :post="post"/>
     </b-col>
   </b-row>
