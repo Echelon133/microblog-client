5d9be729a804b0f2cf4845a959640ea496860e37
Mateusz <echelon133@gmail.com>
2021-02-22 20:57:54 +0100

    Add vue-axios dependency

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
