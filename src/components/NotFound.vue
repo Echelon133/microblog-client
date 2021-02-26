4a7aa0ded36a385c250b2a6122a9faaabede38ca
Mateusz <echelon133@gmail.com>
2021-02-26 23:47:45 +0100

    Add NotFound component (404 page)

---
+++ src/components/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-row>
+    <b-col class="text-center not-found-box">
+      <p class="not-found-number mb-0">404</p>
+      <p class="not-found-text">Nie znaleziono strony</p>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'Not Found'
+}
+</script>
+
+<style scoped>
+.not-found-box {
+  padding-top:200px;
+}
+
+.not-found-number {
+  font-size: 35px;
+}
+
+.not-found-text {
+  font-size: 45px;
+}
+</style>
