f83f26ff2e2effcb90dc2e6e2244520ff63b92fa
Mateusz <echelon133@gmail.com>
2021-04-03 13:35:30 +0200

    Implement logging in using username & password

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,12 +2,14 @@
   <ValidationObserver v-slot="{ invalid }">
     <form class="signin" @submit.prevent="onSubmit">
       <h1 class="h3 mb-3 fw-normal">Zaloguj</h1>
-      <ValidationProvider name="email" rules="required|email" v-slot="{ errors }">
-        <label for="inputEmail" class="visually-hidden mt-2">Adres email</label>
+      <b-alert show v-if="success" variant="success">Logowanie udane</b-alert>
+      <b-alert show v-if="failure" variant="danger">Logowanie nieudane</b-alert>
+      <ValidationProvider name="username" rules="required" v-slot="{ errors }">
+        <label for="inputUsername" class="visually-hidden mt-2">Nazwa użytkownika</label>
         <input
-          v-model="email"
+          v-model="username"
           v-bind:class="{invalidval: errors.length > 0}"
-          type="email" id="inputEmail" class="form-control mt-2" placeholder="Email" autofocus="">
+          type="text" id="inputUsername" class="form-control mt-2" placeholder="Nazwa użytkownika" autofocus="">
       </ValidationProvider>
       <ValidationProvider name="password" rules="required" v-slot="{ errors }">
         <label for="inputPassword" class="visually-hidden mt-2">Hasło</label>
@@ -16,7 +18,6 @@
           v-bind:class="{invalidval: errors.length > 0}"
           type="password" id="inputPassword" class="form-control  mt-2" placeholder="Hasło">
       </ValidationProvider>
-
       <button class="w-100 btn btn-lg btn-primary mt-5" type="submit" :disabled="invalid">Zaloguj</button>
       <p class="mt-5 mb-3 text-muted">© 2021</p>
     </form>
@@ -25,10 +26,9 @@
 
 <script>
 import { ValidationObserver, ValidationProvider, extend } from 'vee-validate'
-import { required, email } from 'vee-validate/dist/rules'
+import { required } from 'vee-validate/dist/rules'
 
 extend('required', required)
-extend('email', email)
 
 export default {
   components: {
@@ -36,12 +36,42 @@ export default {
   },
   data () {
     return {
-      email: null,
-      password: null
+      username: null,
+      password: null,
+      failure: false,
+      success: false
     }
   },
   methods: {
     onSubmit () {
+      // get accessToken and refreshToken from the auth server
+      this.axios.post('http://localhost:8090/api/token', {}, {
+        withCredentials: true,
+        auth: {
+          username: this.username,
+          password: this.password
+        }
+      })
+        .then(() => {
+          // get the logged user object from the resource server
+          this.axios.get('http://localhost:8080/api/users/me', { withCredentials: true })
+            .then((response) => {
+              // cache the logged user object in the local storage
+              localStorage.setItem('user', JSON.stringify(response.data))
+              this.failure = false
+              this.success = true
+              this.username = ''
+              this.password = ''
+              // redirect to the main page
+              setTimeout(() => this.$router.push({path: '/'}), 2000)
+            })
+            .catch(() => {
+              this.failure = true
+            })
+        })
+        .catch(() => {
+          this.failure = true
+        })
     }
   }
 }
