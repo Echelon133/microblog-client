00574215a9e22012f8ebe4e496cf3e4268b3adf7
Mateusz <echelon133@gmail.com>
2021-05-10 22:47:21 +0200

    Change the position of columns that represent posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,9 @@
 <template>
   <b-row class="posts">
-    <b-col sm="9" offset-sm="1" class="text-center pt-5" v-if="posts.length === 0">
+    <b-col sm="12" md="10" offset-md="1" class="text-center pt-5" v-if="posts.length === 0">
       <span class="no-content-text">{{ $t('postList.noPosts') }}</span>
     </b-col>
-    <b-col sm="9" offset-sm="1" class="pt-5" v-else>
+    <b-col sm="12" md="10" offset-md="1" class="pt-5" v-else>
       <Post v-for="post in posts" :key="post.uuid" :post="post"/>
     </b-col>
   </b-row>
